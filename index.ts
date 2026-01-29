93437415465206e228decf5c01a0116727c7fd4a
sanvir-islam <sanvirofficials@gmail.com>
2026-01-29 17:33:23 +0600

    not

--- index.ts
+++ index.ts
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 5000;
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-      })
+      }),
     );
 
     app.use(cookieParser());
@@ -58,7 +58,6 @@ const PORT = process.env.PORT || 5000;
     // --- START SERVER ---
     app.listen(PORT, () => {
       console.log(`✅ Server running on port ${PORT}`);
-      console.log(`👉 Test Link: http://localhost:${PORT}/api/v1/hello`);
 
       // Connect to DB in background
       dbConnect();
