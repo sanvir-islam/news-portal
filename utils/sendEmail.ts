0c81b76d88a3f2364180eebea8f6237f6b2e5d7c
sanvir-islam <sanvirofficials@gmail.com>
2026-02-04 20:39:17 +0600

    Testing auto deploy

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { verificationEmailTemplate } from "../templates/verificationEmail";
 
-// 1. Initialize Resend.
+// 1. Initialize Resend
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 type EmailTemplate = "verification" | "passwordReset";
