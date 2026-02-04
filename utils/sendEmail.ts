9af52a58c4ec1f9b788d12baf3fa7093c717bbe2
sanvir-islam <sanvirofficials@gmail.com>
2026-02-04 20:19:28 +0600

    Fix sender email domain

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -32,7 +32,7 @@ export async function sendEmail(to: string, type: EmailTemplate, data: string) {
     const emailHtml = getEmailTemplate(type, data);
 
     const response = await resend.emails.send({
-      from: "noreply@protidinjonotarnews.com",
+      from: "noreply@api.protidinjonotarnews.com",
       to: to,
       subject: subject,
       html: emailHtml,
